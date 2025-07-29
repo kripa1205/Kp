e61a02110d0554e86bba4136b4f483bde46bca62
kripa1205 <panchalkripa0@gmail.com>
2025-07-29 18:58:46 +0530

    Update products.jsx

--- client /pages/products.jsx
+++ client /pages/products.jsx
@@ -1,40 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import ProductCard from '../components/ProductCard';
-import { Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
+import { Container, Row, Col } from 'react-bootstrap';
+import ProductCard from '../components/ProductCard';
 
-function Products() {
+const Products = () => {
   const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        axios.get('http://localhost:5000/api/products')
-              .then(res => setProducts(res.data))
-                    .catch(err => console.error('Error loading products', err));
-                      }, []);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get('http://localhost:5000/api/products');
+        setProducts(res.data);
+      } catch (err) {
+        console.error('Error fetching products:', err);
+      }
+    };
 
-                        return (
-                            <>
-                                  <Navbar />
-                                        <Container className="my-5">
-                                                <h3 className="mb-4">All Products</h3>
-                                                        <Row>
-                                                                  {products.map((item, idx) => (
-                                                                              <Col md={4} className="mb-4" key={idx}>
-                                                                                            <ProductCard
-                                                                                                            id={item._id}
-                                                                                                                            name={item.name}
-                                                                                                                                            price={item.price}
-                                                                                                                                                            img={item.img}
-                                                                                                                                                                          />
-                                                                                                                                                                                      </Col>
-                                                                                                                                                                                                ))}
-                                                                                                                                                                                                        </Row>
-                                                                                                                                                                                                              </Container>
-                                                                                                                                                                                                                    <Footer />
-                                                                                                                                                                                                                        </>
-                                                                                                                                                                                                                          );
-                                                                                                                                                                                                                          }
+    fetchProducts();
+  }, []);
 
-                                                                                                                                                                                                                          export default Products;
+  return (
+    <Container className="py-4">
+      <h2 className="mb-4">Our Collection</h2>
+      <Row>
+        {products.length === 0 ? (
+          <p>No products found.</p>
+        ) : (
+          products.map((product) => (
+            <Col key={product._id} sm={6} md={4} lg={3} className="mb-4">
+              <ProductCard product={product} />
+            </Col>
+          ))
+        )}
+      </Row>
+    </Container>
+  );
+};
+
+export default Products;
