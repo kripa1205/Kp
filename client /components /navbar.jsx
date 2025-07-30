76dde1c21a3583c274e2ba9d74e7d98497dedb84
kripa1205 <panchalkripa0@gmail.com>
2025-07-30 08:42:53 +0530

    Update navbar.jsx

--- client /components /navbar.jsx
+++ client /components /navbar.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 
 function MainNavbar() {
-  return (
-      <Navbar bg="light" expand="lg" className="shadow-sm">
-            <Container>
-                    <Navbar.Brand href="/" style={{ fontFamily: 'Playfair Display, serif', fontWeight: '600' }}>
-                              Rose Luxe
-                                      </Navbar.Brand>
-                                              <Navbar.Toggle />
-                                                      <Navbar.Collapse>
-                                                                <Nav className="ms-auto">
-                                                                            <Nav.Link href="/">Home</Nav.Link>
-                                                                                        <Nav.Link href="/products">Shop</Nav.Link>
-                                                                                                    <Nav.Link href="/cart">Cart</Nav.Link>
-                                                                                                                <Nav.Link href="/login">Login</Nav.Link>
-                                                                                                                          </Nav>
-                                                                                                                                  </Navbar.Collapse>
-                                                                                                                                        </Container>
-                                                                                                                                            </Navbar>
-                                                                                                                                              );
-                                                                                                                                              }
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
-                                                                                                                                              export default MainNavbar;
+  const handleLogout = () => {
+    localStorage.removeItem('userInfo');
+    window.location.href = '/';
+  };
+
+  return (
+    <Navbar bg="light" expand="lg" className="shadow-sm">
+      <Container>
+        <Navbar.Brand href="/" style={{ fontFamily: 'Playfair Display, serif', fontWeight: '600' }}>
+          Rose Luxe
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/products">Shop</Nav.Link>
+            <Nav.Link href="/cart">Cart</Nav.Link>
+
+            {userInfo ? (
+              <>
+                <Nav.Link href="/dashboard">
+                  👋 {userInfo.name.split(' ')[0]}
+                </Nav.Link>
+                <Button variant="outline-dark" size="sm" onClick={handleLogout}>
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <Nav.Link href="/login">Login</Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MainNavbar;
