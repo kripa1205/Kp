8c52a85455b0034f8bdaa3752d572b597be5e0c3
kripa1205 <panchalkripa0@gmail.com>
2025-07-30 08:31:37 +0530

    Update userroutes.js

--- server /routes/userroutes.js
+++ server /routes/userroutes.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import {
   registerUser,
-    loginUser,
-      getUserProfile,
-      } from '../controllers/userController.js';
-      import protect from '../middleware/authMiddleware.js';
+  loginUser,
+  getUserProfile,
+} from '../controllers/userController.js';
+import protect from '../middleware/authMiddleware.js';
 
-      const router = express.Router();
+const router = express.Router();
 
-      router.post('/', registerUser);
-      router.post('/login', loginUser);
-      router.get('/profile', protect, getUserProfile);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.get('/profile', protect, getUserProfile);
 
-      export default router;
+export default router;
