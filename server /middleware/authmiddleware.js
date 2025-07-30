b0be2fcca0e103593dac0d5d221ce932c8a8202c
kripa1205 <panchalkripa0@gmail.com>
2025-07-30 08:33:54 +0530

    Update authmiddleware.js

--- server /middleware/authmiddleware.js
+++ server /middleware/authmiddleware.js
@@ -5,18 +5,18 @@ import config from '../utils/config.js';
 const protect = async (req, res, next) => {
   let token = req.headers.authorization;
 
-    if (token && token.startsWith('Bearer')) {
-        try {
-              token = token.split(' ')[1];
-                    const decoded = jwt.verify(token, config.jwtSecret);
-                          req.user = await User.findById(decoded.id).select('-password');
-                                next();
-                                    } catch (err) {
-                                          res.status(401).json({ message: 'Token failed or expired' });
-                                              }
-                                                } else {
-                                                    res.status(401).json({ message: 'Not authorized, no token' });
-                                                      }
-                                                      };
+  if (token && token.startsWith('Bearer')) {
+    try {
+      token = token.split(' ')[1];
+      const decoded = jwt.verify(token, config.jwtSecret);
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (err) {
+      res.status(401).json({ message: 'Token failed or expired' });
+    }
+  } else {
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
 
-                                                      export default protect;
+export default protect;
