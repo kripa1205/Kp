937766d4861f7ea2f20a03a5ebcd3ea482691feb
kripa1205 <panchalkripa0@gmail.com>
2025-07-30 15:14:55 +0530

    Update index.js

--- server /index.js
+++ server /index.js
@@ -11,19 +11,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json()); // to read JSON from req.body
+app.use(express.json());
 
 connectDB();
 
-// Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 
-// Home route
 app.get('/', (req, res) => {
   res.send('Server is running...');
-  });
+});
 
-  app.listen(PORT, () => {
-    console.log(`Server is live at http://localhost:${PORT}`);
-    });
+app.listen(PORT, () => {
+  console.log(`Server is live at http://localhost:${PORT}`);
+});
