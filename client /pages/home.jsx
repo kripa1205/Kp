7aa73cad1f45b498c6c95bc2965f6c03131d4703
kripa1205 <panchalkripa0@gmail.com>
2025-07-30 09:00:49 +0530

    Update home.jsx

--- client /pages/home.jsx
+++ client /pages/home.jsx
@@ -4,17 +4,29 @@ import HeroBanner from '../components/HeroBanner';
 import AdCarousel from '../components/AdCarousel';
 import FeaturedCarousel from '../components/FeaturedCarousel';
 import Footer from '../components/Footer';
+import { Container } from 'react-bootstrap';
 
 function Home() {
   return (
-      <>
-            <Navbar />
-                  <HeroBanner />
-                        <AdCarousel />
-                              <FeaturedCarousel />
-                                    <Footer />
-                                        </>
-                                          );
-                                          }
+    <>
+      <Navbar />
 
-                                          export default Home;
+      {/* Hero Banner full width */}
+      <HeroBanner />
+
+      {/* Advertisement Carousel */}
+      <Container fluid className="my-4">
+        <AdCarousel />
+      </Container>
+
+      {/* Featured Products Carousel */}
+      <Container fluid className="my-5">
+        <FeaturedCarousel />
+      </Container>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
